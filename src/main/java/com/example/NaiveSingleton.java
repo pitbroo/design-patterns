d038117ce57bc8e9dc2e22453ebfd623c6f5f556
piotr <piotr0brodziak@gmail.com>
2023-10-29 00:12:39 +0200

    naive singleton

---
+++ src/main/java/com/example/NaiveSingleton.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class NaiveSingleton {
+    private static NaiveSingleton instance;
+
+    private NaiveSingleton() {
+    }
+
+    public static NaiveSingleton getInstance() {
+        if (instance == null) {
+            instance = new NaiveSingleton();
+        }
+        return instance;
+    }
+}
+
